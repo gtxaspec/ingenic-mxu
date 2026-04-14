8a2d5020a84076087d029fe2f3a63405a31374ee
gtxaspec <gtxent@gmail.com>
2026-04-13 23:41:22 -0700

    expand smoke test to 25 tests - all pass on T31
    Added: add/sub/mul_h, add/sub_d, adduu_w, ceqz_w, bcnt_w, norv,
    sub_b, mins_w, madd_w, mods_w, clts_w - all correct on device.

--- test_builtins_quick.c
+++ test_builtins_quick.c
@@ -155,6 +155,159 @@ int main(void) {
         check_v("xorv", outb, exp, 16);
     }
 
+    /* --- Halfword ops --- */
+    {
+        short A_H[8] __attribute__((aligned(16))) = {100, -100, 0, 32767, -32768, 1000, 255, -1};
+        short B_H[8] __attribute__((aligned(16))) = {200,   50, 0,     1,      1,  500, 256,  1};
+        v8i16 a = *(v8i16*)A_H;
+        v8i16 b = *(v8i16*)B_H;
+
+        v8i16 r = __builtin_mxu2_add_h(a, b);
+        short outh[8] __attribute__((aligned(16)));
+        *(v8i16*)outh = r;
+        short exph[8];
+        for (int i = 0; i < 8; i++) exph[i] = (short)(A_H[i] + B_H[i]);
+        check_v("add_h", outh, exph, 16);
+
+        r = __builtin_mxu2_sub_h(a, b);
+        *(v8i16*)outh = r;
+        for (int i = 0; i < 8; i++) exph[i] = (short)(A_H[i] - B_H[i]);
+        check_v("sub_h", outh, exph, 16);
+
+        r = __builtin_mxu2_mul_h(a, b);
+        *(v8i16*)outh = r;
+        for (int i = 0; i < 8; i++) exph[i] = (short)(A_H[i] * B_H[i]);
+        check_v("mul_h", outh, exph, 16);
+    }
+
+    /* --- Doubleword ops --- */
+    {
+        long long A_D[2] __attribute__((aligned(16))) = {100LL, -50LL};
+        long long B_D[2] __attribute__((aligned(16))) = {200LL,  25LL};
+        v2i64 a = *(v2i64*)A_D;
+        v2i64 b = *(v2i64*)B_D;
+
+        v2i64 r = __builtin_mxu2_add_d(a, b);
+        long long outd[2] __attribute__((aligned(16)));
+        *(v2i64*)outd = r;
+        long long expd[2] = {300LL, -25LL};
+        check_v("add_d", outd, expd, 16);
+
+        r = __builtin_mxu2_sub_d(a, b);
+        *(v2i64*)outd = r;
+        long long expd2[2] = {-100LL, -75LL};
+        check_v("sub_d", outd, expd2, 16);
+    }
+
+    /* --- Unsigned saturating --- */
+    {
+        unsigned int AU[4] __attribute__((aligned(16))) = {0xFFFFFFF0, 100, 0, 50};
+        unsigned int BU[4] __attribute__((aligned(16))) = {0x20, 200, 10, 100};
+        v4u32 a = *(v4u32*)AU;
+        v4u32 b = *(v4u32*)BU;
+        v4u32 r = __builtin_mxu2_adduu_w(a, b);
+        unsigned int outu[4] __attribute__((aligned(16)));
+        *(v4u32*)outu = r;
+        unsigned int expu[4] = {0xFFFFFFFF, 300, 10, 150}; /* first saturates */
+        check_v("adduu_w", outu, expu, 16);
+    }
+
+    /* --- Compare to zero --- */
+    {
+        v4i32 a = *(v4i32*)A_W;  /* {100, -50, 0x7FFFFFF0, 0} */
+        v4i32 r = __builtin_mxu2_ceqz_w(a);
+        *(v4i32*)out = r;
+        int exp[4];
+        for (int i = 0; i < 4; i++) exp[i] = (A_W[i] == 0) ? -1 : 0;
+        check_v("ceqz_w", out, exp, 16);
+    }
+
+    /* --- Bit counting --- */
+    {
+        v4i32 a = *(v4i32*)A_W;
+        v4i32 r = __builtin_mxu2_bcnt_w(a);
+        *(v4i32*)out = r;
+        int exp[4];
+        for (int i = 0; i < 4; i++) {
+            unsigned v = (unsigned)A_W[i];
+            int c = 0;
+            while (v) { c += v & 1; v >>= 1; }
+            exp[i] = c;
+        }
+        check_v("bcnt_w", out, exp, 16);
+    }
+
+    /* --- Norv (NOR) --- */
+    {
+        v16i8 a = *(v16i8*)A_B;
+        v16i8 b = *(v16i8*)B_B;
+        v16i8 r = __builtin_mxu2_norv(a, b);
+        *(v16i8*)outb = r;
+        unsigned char exp[16];
+        for (int i = 0; i < 16; i++)
+            exp[i] = ~((unsigned char)A_B[i] | (unsigned char)B_B[i]);
+        check_v("norv", outb, exp, 16);
+    }
+
+    /* --- Sub byte --- */
+    {
+        v16i8 a = *(v16i8*)A_B;
+        v16i8 b = *(v16i8*)B_B;
+        v16i8 r = __builtin_mxu2_sub_b(a, b);
+        *(v16i8*)outb = r;
+        signed char exp[16];
+        for (int i = 0; i < 16; i++) exp[i] = (signed char)(A_B[i] - B_B[i]);
+        check_v("sub_b", outb, exp, 16);
+    }
+
+    /* --- Mins/Maxu --- */
+    {
+        v4i32 a = *(v4i32*)A_W;
+        v4i32 b = *(v4i32*)B_W;
+        v4i32 r = __builtin_mxu2_mins_w(a, b);
+        *(v4i32*)out = r;
+        int exp[4];
+        for (int i = 0; i < 4; i++) exp[i] = A_W[i] < B_W[i] ? A_W[i] : B_W[i];
+        check_v("mins_w", out, exp, 16);
+    }
+
+    /* --- Madd (multiply-add) --- */
+    {
+        int C_W[4] __attribute__((aligned(16))) = {10, 20, 30, 40};
+        v4i32 a = *(v4i32*)C_W;
+        v4i32 b = *(v4i32*)A_W;
+        v4i32 c = *(v4i32*)B_W;
+        v4i32 r = __builtin_mxu2_madd_w(a, b, c);
+        *(v4i32*)out = r;
+        int exp[4];
+        for (int i = 0; i < 4; i++) exp[i] = C_W[i] + A_W[i] * B_W[i];
+        check_v("madd_w", out, exp, 16);
+    }
+
+    /* --- Mods (modulo) --- */
+    {
+        int da[4] __attribute__((aligned(16))) = {100, -99, 77, 1000};
+        int db[4] __attribute__((aligned(16))) = {3, 7, -5, 11};
+        v4i32 a = *(v4i32*)da;
+        v4i32 b = *(v4i32*)db;
+        v4i32 r = __builtin_mxu2_mods_w(a, b);
+        *(v4i32*)out = r;
+        int exp[4];
+        for (int i = 0; i < 4; i++) exp[i] = da[i] % db[i];
+        check_v("mods_w", out, exp, 16);
+    }
+
+    /* --- Compare signed less-than --- */
+    {
+        v4i32 a = *(v4i32*)A_W;
+        v4i32 b = *(v4i32*)B_W;
+        v4i32 r = __builtin_mxu2_clts_w(a, b);
+        *(v4i32*)out = r;
+        int exp[4];
+        for (int i = 0; i < 4; i++) exp[i] = (A_W[i] < B_W[i]) ? -1 : 0;
+        check_v("clts_w", out, exp, 16);
+    }
+
     printf("\n=== Results: %d pass, %d fail ===\n", pass_count, fail_count);
     return fail_count > 0 ? 1 : 0;
 }
